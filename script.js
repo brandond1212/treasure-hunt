344dcb22758967692241457e9f50b61bb7b82e3c
brandond1212 <43242495+brandond1212@users.noreply.github.com>
2019-03-07 09:26:42 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+$(".button1").click(function(){
+    $(".button1").text("now double click");
+});
+$(".button1").dblclick(function(){
+    $(".button1").text("now click the right image- to stop the rain");
+});
+$(".blue").click(function(){
+    $(".blue").hide();
+});
+$(".button2").click(function(){
+    $(".button2").text("Now click The Raindow");
+});
